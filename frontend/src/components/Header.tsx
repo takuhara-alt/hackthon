4220ae298d63c950f3e50ecf04bc53c96453224b
takuhara-alt <tazaiyuan9@gmail.com>
2025-02-05 14:57:27 +0900

    settings.tsxのフォントサイズの変更を修正

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
-import { ReactDOM } from 'react';
 import "../App.css";
 
 const Header: React.FC = () => {
@@ -13,9 +11,8 @@ const Header: React.FC = () => {
             <nav className='app-date'>
                 <p>今日の日付: {new Date().toLocaleDateString('ja-JP')}</p>
             </nav>
-
         </div>
-
     );
-    }
-export {Header};
+}
+
+export { Header };
