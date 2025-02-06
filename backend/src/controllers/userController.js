e2c6bf31c6be4d7487bebe4ee8ae792e30607172
たっちゃん <138844357+takuhara-alt@users.noreply.github.com>
2025-02-06 12:22:00 +0900

    Delete backend directory

--- backend/src/controllers/userController.js
+++
@@ -1,5 +0,0 @@
-const getUsers = (req, res) => {
-    res.json({ users: [{ id: 1, name: 'John Doe' }] });
-};
-
-module.exports = { getUsers };
--- backend/src/models/userModel.js
+++
@@ -1,7 +0,0 @@
-const users = [{ id: 1, name: 'John Doe' }];
-
-const getAllUsers = () => {
-    return users;
-};
-
-module.exports = { getAllUsers };
