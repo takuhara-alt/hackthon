f00d2250391c5870360015d12e6e1d65bc2990f4
たっちゃん <138844357+takuhara-alt@users.noreply.github.com>
2025-02-06 12:22:10 +0900

    Delete frontend directory

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders app header", () => {
-  render(<App />);
-  const headerElement = screen.getByText(/日常生活アプリ/i);
-  expect(headerElement).toBeInTheDocument();
-});
